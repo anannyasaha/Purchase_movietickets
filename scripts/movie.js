77750e2d0b85edee7f4edfc6324fb56e13c44c43
root <root@DESKTOP-DF8L8DR.localdomain>
2021-03-26 14:56:10 +0600

    added margin but not working and the fetch is not working

--- scripts/movie.js
+++ scripts/movie.js
@@ -1,6 +1,28 @@
 
 window.onload=function(){
     fetch('http://www.omdbapi.com/?i=tt3896198&apikey=d9703216')
-    .then((response)=>response.json)
-    .then(data=>console.log(data));
+    .then((response)=>response.json())
+    .then(function(data){
+        //var image=document.getElementById("image");
+        //image.src=data.Poster;
+        console.log(data)
+        var title=document.getElementById("title");
+        title.value=data.Title;
+        var year=document.getElementById("year");
+        year.value=data.Year;
+        var genre=document.getElementById("genre");
+        genre.value=data.Genre;
+        var runtime=document.getElementById("runtime");
+        runtime.value=data.Runtime;
+        var director=document.getElementById("director");
+        director.value=data.Director;
+        var writer=document.getElementById("writer");
+        writer.value=data.Writer;
+        var actor=document.getElementById("Actors");
+        actor.value=data.Actors;
+        var plot=document.getElementById("plot");
+        plot.value=data.Plot;
+
+    }
+        );
 }

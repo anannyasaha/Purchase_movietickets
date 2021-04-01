f5943de77efe7c188d3b9da3c935d0652b790a84
root <root@DESKTOP-DF8L8DR.localdomain>
2021-04-01 19:09:30 +0600

    movie details are updated

--- basic.js
+++ basic.js
@@ -7,12 +7,14 @@ app.use('/', function(request, response, next) {
     console.log('HTML file being generated.');
     next();
 });
-app.get('/', function(request,response){
+app.get('/', function(request,response,next){
     response.sendFile(__dirname+'/public/showtimes.html');
+    next();
 })
-app.get('/', function(request, response) { 
+app.get('/', function(request, response,next) { 
     console.log(request.query);
     response.send("data entered");
+    next();
 });
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function() {
--- public/scripts/showtimes.js
+++ public/scripts/showtimes.js
@@ -10,14 +10,67 @@ window.onload=function(){
     for(var i=0;i<data.length;i++){
         if(data[i].date=="2021/01/01"){
         var row=table.insertRow(i);
+        row.setAttribute("id","table_rows");
         var title=row.insertCell(0);
+        title.setAttribute("class","moviecell");
         title.innerHTML=data[i].title;
         var time=row.insertCell(1);
-        time.innerHTML=data[i].times;
-        console.log(data[i].times);
+        time.setAttribute("id","cell");
+        for(var j=0;j<data[i].times.length;j++){
+            var showtime=document.createElement("ul");
+            showtime.innerHTML=data[i].times[j];
+            time.append(showtime);
+            console.log(showtime);
+        }
         }
     }
+    $('.moviecell').on('click',
+    function(){
+        
+          var table_form=document.getElementById('field_form');
+          table_form.style.display='block';
+          for(var i =0;i<data.length;i++){
+              if(this.innerHTML==data[i].title){
+                fetch('https://www.omdbapi.com/?i='+data[i].id+'&apikey=d9703216')
+                .then((response)=>response.json())
+                .then(function(data){
+                    const div = document.getElementById('rating');
+
+                    while(div.firstChild){
+                   div.removeChild(div.firstChild);
+                   }
+                    var image=document.getElementById("image");
+                    image.src=data.Poster;
+                    var title=document.getElementById("title");
+                    title.value=data.Title;
+                    var year=document.getElementById("year");
+                    year.value=data.Year;
+                    var genre=document.getElementById("genre");
+                    genre.value=data.Genre;
+                    var runtime=document.getElementById("runtime");
+                    runtime.value=data.Runtime;
+                    var director=document.getElementById("director");
+                    director.value=data.Director;
+                    var writer=document.getElementById("writer");
+                    writer.value=data.Writer;
+                    var actor=document.getElementById("Actors");
+                    actor.value=data.Actors;
+                    var plot=document.getElementById("plot");
+                    plot.value=data.Plot;
+                    var imdb=data.imdbRating;
+                    var rating=document.getElementById("rating");
+                   
+                    for(var i=0;i<imdb;i++){
+                        var img = document.createElement("img");
+                        img.src = "images/trophy.png";
+                     rating.appendChild(img);
+                     console.log(rating);        }
+                })
+            }
+          }
+        })
     });
     
 }
 }
+
